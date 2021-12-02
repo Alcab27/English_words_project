e08f85a856d47f57d59adfed8937e187665cd036
Alan Cabrera <holasoyalan9@gmail.com>
2021-12-01 19:34:52 -0600

    Agregamos listas con las palabras

--- learn_words.py
+++ learn_words.py
@@ -1,5 +1,16 @@
 def run():
-    with open('')
+    english_words = []
+    spanish_words = []
+
+    with open('./archivos/english_words.txt', 'r') as f:
+        for i in f:
+            english_words.append(i)
+    with open('./archivos/spanish_words.txt', 'r') as l:
+        for i in l:
+            spanish_words.append(i)
+
+    
+
 
 
 if __name__ == '__main__':
