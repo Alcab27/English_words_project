89d9e9354e789e84bd8739353716f2ea3243075b
Alan Cabrera <holasoyalan9@gmail.com>
2021-12-03 00:07:11 -0600

    Volvimos al proyecto básico

--- learn_words.py
+++ learn_words.py
@@ -23,11 +23,11 @@ def run():
         num_eng_words = 0
         num_esp_words = 0
 
-        with open('./archivos/totests.txt', 'r', encoding='utf-8') as f:
+        with open('./archivos/english_words.txt', 'r', encoding='utf-8') as f:
             for i in f:
                 english_words.append(i.replace('\n', ''))
                 num_eng_words += 1
-        with open('./archivos/papruebas.txt', 'r', encoding='utf-8') as l:
+        with open('./archivos/spanish_words.txt', 'r', encoding='utf-8') as l:
             for i in l:
                 spanish_words.append(i.replace('\n', ''))
                 num_esp_words += 1
@@ -40,8 +40,6 @@ def run():
 
         if palabra == spanish_words[random_num]:
             print('Sí!')
-            spanish_words.pop(random_num)
-            english_words.pop(random_num)
         else:
             print('No')
             print(spanish_words[random_num]) 
