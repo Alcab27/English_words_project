ba816c7c6892e8ffb29b1848255fedcdc7ce2c50
Alan Cabrera <holasoyalan9@gmail.com>
2021-12-03 00:12:34 -0600

    Pseudo hace lo que queremos jaja

--- learn_words.py
+++ learn_words.py
@@ -17,21 +17,20 @@ def get_word_user():
 
 def run():
     borrarPantasha()
+    english_words = []
+    spanish_words = []
+    num_eng_words = 0
+    num_esp_words = 0
+
+    with open('./archivos/totests.txt', 'r', encoding='utf-8') as f:
+        for i in f:
+            english_words.append(i.replace('\n', ''))
+            num_eng_words += 1
+    with open('./archivos/papruebas.txt', 'r', encoding='utf-8') as l:
+        for i in l:
+            spanish_words.append(i.replace('\n', ''))
+            num_esp_words += 1
     while True:
-        english_words = []
-        spanish_words = []
-        num_eng_words = 0
-        num_esp_words = 0
-
-        with open('./archivos/english_words.txt', 'r', encoding='utf-8') as f:
-            for i in f:
-                english_words.append(i.replace('\n', ''))
-                num_eng_words += 1
-        with open('./archivos/spanish_words.txt', 'r', encoding='utf-8') as l:
-            for i in l:
-                spanish_words.append(i.replace('\n', ''))
-                num_esp_words += 1
-
         random_num = random.randint(0,num_eng_words - 1)
 
         print(english_words[random_num])
@@ -40,6 +39,8 @@ def run():
 
         if palabra == spanish_words[random_num]:
             print('Sí!')
+            spanish_words.pop(random_num)
+            english_words.pop(random_num)
         else:
             print('No')
             print(spanish_words[random_num]) 
