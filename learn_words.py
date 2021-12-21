2c08d740b13953841c4884d41275ff219aa32c1e
Alan Cabrera <holasoyalan9@gmail.com>
2021-12-20 19:34:00 -0600

    Agregamos una lista con las palabras equivocadas para así prácticar solo esas

--- learn_words.py
+++ learn_words.py
@@ -1,7 +1,6 @@
 import os 
 import random
 import time
-from typing import Counter 
 
 def borrarPantasha():
     if os.name == 'posix':
@@ -18,16 +17,19 @@ def run():
     borrarPantasha()
     english_words = []
     spanish_words = []
-    palabras_erradas = []
+    palabras_erradas_english = []
+    palabras_erradas_spanish = []
     num_eng_words = 0
     num_esp_words = 0
     FASHAMOS = bool
 
     with open('./archivos/adverbs_nouns/english_wordsAN.txt', 'r', encoding='utf-8') as f:
+    # with open('./archivos/erradas_eng.txt', 'r', encoding='utf-8') as f:
         for i in f:
             english_words.append(i.replace('\n', ''))
             num_eng_words += 1
     with open('./archivos/adverbs_nouns/spanish_wordsAN.txt', 'r', encoding='utf-8') as l:
+    # with open('./archivos/erradas_esp.txt', 'r', encoding='utf-8') as l:
         for i in l:
             spanish_words.append(i.replace('\n', ''))
             num_esp_words += 1
@@ -47,8 +49,17 @@ def run():
             else:
                 print('No')
                 print(spanish_words[random_num]) 
-                palabras_erradas.append(english_words[random_num])
+                palabras_erradas_english.append(english_words[random_num])
+                palabras_erradas_spanish.append(spanish_words[random_num])
                 FASHAMOS = True
+                with open('./archivos/erradas_eng.txt', 'w', encoding='utf-8') as e:
+                    for words in palabras_erradas_english:
+                        e.write(words)
+                        e.write('\n')
+                with open('./archivos/erradas_esp.txt', 'w', encoding='utf-8') as s:
+                    for words in palabras_erradas_spanish:
+                        s.write(words)
+                        s.write('\n')
 
 
             time.sleep(0.5)
@@ -60,12 +71,12 @@ def run():
             print('Acabamooos!')
             if FASHAMOS == True:
                 print('Erraste en las siguientes palabras: ')
-                for i in palabras_erradas:
-                    err = int(palabras_erradas.count(i))
-                    print(i, palabras_erradas.count(i))
-                    if palabras_erradas.count(i) > 1:
+                for i in palabras_erradas_english:
+                    err = int(palabras_erradas_english.count(i))
+                    print(i, palabras_erradas_english.count(i))
+                    if palabras_erradas_english.count(i) > 1:
                         for l in range(0, err):
-                            palabras_erradas.remove(i)
+                            palabras_erradas_english.remove(i)
             else:
                 print('Felicidades!! No erraste ninguna palabra')
 
